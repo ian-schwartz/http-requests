528dced10a55ae458c2b008eeb1eb3235c7a243a
Ian Schwartz <ian.schwartz23@gmail.com>
2020-07-15 12:10:12 -0700

    axios default global config

--- http-requests-app/src/components/FullPost/NewPost/NewPost.js
+++ http-requests-app/src/components/FullPost/NewPost/NewPost.js
@@ -16,7 +16,7 @@ class NewPost extends Component {
       author: this.state.author,
     };
     axios
-      .post("https://jsonplaceholder.typicode.com/posts/", data)
+      .post("/posts/", data)
       .then((res) => {
         console.log(res);
       });
