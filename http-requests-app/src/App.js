8524e79ec62342cd8ce8173f9b3fef690fa610eb
Ian Schwartz <ian.schwartz23@gmail.com>
2020-08-24 21:15:43 -0700

    basename when serving from sub-dir

--- http-requests-app/src/App.js
+++ http-requests-app/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
+    // <Router basename="my-app">
     <Router>
       <div className="App">
         <Blog />
